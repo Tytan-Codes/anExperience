7bca53721362c191e38ba114731cb5eb4cacfbb0
Tytan <tytan@tytan.dev>
2025-02-21 18:36:27 -0500

    Enhance ExperienceTwo page with refined GSAP animation and layout updates

--- app/experienceTwo/page.tsx
+++ app/experienceTwo/page.tsx
@@ -15,24 +15,23 @@ export default function ExperienceTwo() {
         const tl = gsap.timeline()
         tl.to(".animation-1", {
             opacity: 1,
+            duration: 1,
+            ease: "power2.out",
             y: 0,
         })
-        tl.to(".animation-1", {
-            opacity: 1,
-            y: -100,
-            duration: 1,
-            ease: "power2.out"
-        }, "+=1")
     })
 
     return (
-        <div className="flex flex-col  bg-gradient-to-b from-zinc-950 to-zinc-800">
+        <div className="flex flex-col  bg-zinc-950">
             <div className="flex flex-col items-center justify-center h-screen">
-                <div className="border-b border-white/20 p-20">
-                <h1 className="text-9xl font-bold text-white font-manuscribe">Tytan Codes</h1>
-                {/* Fucking do this shit
-                Put shit so its like a grid and so text underline then more shit
-                 */}
+                <div className="animation-1 border-b border-white/20 p-20 flex flex-col items-center justify-center">
+                    <h1 className="text-9xl font-bold text-white font-manuscribe pb-2">Tytan Codes</h1>
+                    <span className="text-white/50 text-lg font-emberly">Scroll down</span>
+                </div>
+            </div>
+            <div className="flex flex-col h-screen w-screen">
+                <div className="flex">
+                    
                 </div>
             </div>
         </div>
