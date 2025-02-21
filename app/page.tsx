e59384f55a968a9a977462c587801958b05a0d18
Tytan <tytan@tytan.dev>
2025-02-21 15:02:31 -0500

    Remove unused useState import in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useRouter } from 'next/navigation';
-import { useState, useEffect, useRef } from 'react';
+import {  useEffect, useRef } from 'react';
 import gsap from 'gsap';
 
 export default function Home() {
